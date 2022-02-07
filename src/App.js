fddfd65e79061c540c554ea5810657c9baad7818
HOKlNG <ghtjd22103@gmail.com>
2022-02-08 01:24:32 +0900

    movie page with data

--- src/App.js
+++ src/App.js
@@ -1,49 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
+
 
 class App extends React.Component{
-  constructor(props) {
-    super(props);
-    console.log('constructor')
-  }
-
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
 
-  add = () => {
-    this.setState(current => ({
-       count: current.count +1, 
-      }));
+  getMovies = async() => {
+    const {
+      data:{
+        data: {movies}
+      }
+    } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_py=rating');
+    this.setState({ movies: movies, isLoading: false})
   }
 
-  minus = () => {
-    this.setState(current => ({
-       count: current.count -1 
-      }));
-  }
-  
-  componentDidMount(){
-    console.log('didmount')
-  }
-
-  componentDidUpdate(){
-    console.log('didupdate')
-  }
-
-  componentWillUnmount(){
-    console.log('will unmount')
+  componentDidMount() {
+    // 영화 데이터 로딩
+    this.getMovies();
   }
 
   render() {
-    console.log('render');
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    )
+    <section class="container">
+      {isLoading ? (
+        <div class="loader">
+          <span class="loader__text">Loading ....</span>
+        </div>
+      ) : (
+        <div class="movies">
+          {movies.map(movie => (
+            <Movie 
+              key = {movie.id} //에러메시지 방지용
+              id = {movie.id}
+              year = {movie.year}
+              title = {movie.title}
+              summary = {movie.summary}
+              poster = {movie.medium_cover_image}
+            />
+          ))}
+        </div>
+      )}
+    </section>
+    );
   }
 }
 export default App;
