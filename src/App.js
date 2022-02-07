98f26ce5c57e1b0c667ef7cd6630478abbcc6ce9
HOKlNG <ghtjd22103@gmail.com>
2022-02-08 00:26:30 +0900

    component life cycle

--- src/App.js
+++ src/App.js
@@ -1,54 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Food({name, picture, rating}){
-  
-  return (
-    <div>
-      <h2> I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name}/>
-    </div>
-  )
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: 'Kimchi',
-    image: 'https://health.chosun.com/site/data/img_dir/2021/09/01/2021090100998_0.jpg',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: 'Samgyeopsal',
-    image: 'https://cdn.mindgil.com/news/photo/202103/70839_7148_1250.jpg',
-    rating: 4,
-  },
-  {
-    id: 3,
-    name: 'Bibimbap',
-    image: 'https://health.chosun.com/site/data/img_dir/2021/01/27/2021012702508_0.jpg',
-    rating: 4.5,
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    console.log('constructor')
   }
-];
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
+  state = {
+    count: 0,
+  };
 
-    </div>
-  )
+  add = () => {
+    this.setState(current => ({
+       count: current.count +1, 
+      }));
+  }
+
+  minus = () => {
+    this.setState(current => ({
+       count: current.count -1 
+      }));
+  }
+  
+  componentDidMount(){
+    console.log('didmount')
+  }
+
+  componentDidUpdate(){
+    console.log('didupdate')
+  }
+
+  componentWillUnmount(){
+    console.log('will unmount')
+  }
+
+  render() {
+    console.log('render');
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
 }
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating : PropTypes.number.isRequired,
-}
-
 export default App;
